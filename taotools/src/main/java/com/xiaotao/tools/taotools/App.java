b42e7139005f28db592b364df70f50a43b1ab82e
xiaotao_405 <xiaotao_405@126.com>
2019-12-15 17:01:48 +0800

    新增jdbc样例

--- taotools/src/main/java/com/xiaotao/tools/taotools/App.java
+++ taotools/src/main/java/com/xiaotao/tools/taotools/App.java
@@ -1,5 +1,8 @@
 package com.xiaotao.tools.taotools;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Hello world!
  *
@@ -9,5 +12,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        List l = new ArrayList<String>();
     }
 }
