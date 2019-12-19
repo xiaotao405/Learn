7a11fc196ad258252e046988ab16c510b59e6f72
xiaotao_405 <xiaotao_405@126.com>
2019-12-19 23:03:13 +0800

    rename

--- taotools/src/test/java/com/xiaotao/tools/junit/Junit4SuiteCalculateTest.java
+++ taotools/src/test/java/com/xiaotao/tools/junit/Junit4SuiteCalculateTest.java
@@ -6,6 +6,6 @@ import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({ Junit4DemoTest.class ,Junit4Demo2Test.class})
-public class Junit4SuiteTest {
+public class Junit4SuiteCalculateTest {
 
 }
